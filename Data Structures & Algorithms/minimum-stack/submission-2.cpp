30db7d35622aaa8db5cd53fd371f1f55d0f70366
Maharshi <96075286+Maharshi007@users.noreply.github.com>
2026-06-02 22:59:05 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,42 @@
+class MinStack {
+    stack<long long> st;
+    long long mini = INT_MAX;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push(val);
+            mini = val;
+        }
+        else{
+
+            if(val >= mini) st.push(val);
+            else{
+                st.push(2LL*val - mini);
+                mini = val;
+            }
+        }
+    }
+    
+    void pop() {
+        long long x = st.top();
+         st.pop();
+        if(x<mini) {
+            mini = 2LL* mini - x; 
+        }
+    }
+    
+    int top() {
+        long long x = st.top();
+        if(x<mini) return mini;
+        return x;
+        
+    }
+    
+    int getMin() {
+        return mini;
+    }
+};
