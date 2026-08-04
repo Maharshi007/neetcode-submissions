699ffb29b6cda32ae10391c5ffe31791f2ab203c
Maharshi <96075286+Maharshi007@users.noreply.github.com>
2026-08-04 16:31:08 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    void helper(vector<int>& nums,int index,int n, vector<int>& temp, vector<vector<int>>& ans)
+    {
+        if(index == n)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[index]);
+        helper(nums,index+1,n,temp,ans);
+        temp.pop_back();
+        helper(nums,index+1,n,temp,ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        helper(nums,0,nums.size(),temp,ans);
+        return ans;
+    }
+};
