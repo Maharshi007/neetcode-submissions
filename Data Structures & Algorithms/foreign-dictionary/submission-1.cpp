2bc27fb2d03e528d18754ff04b18943f7d87f005
Maharshi <96075286+Maharshi007@users.noreply.github.com>
2026-08-12 12:09:14 +0530

    Add: foreign-dictionary - submission-1

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-1.cpp
@@ -0,0 +1,59 @@
+class Solution {
+   private:
+    bool topoSort(int node, vector<int>& vis, stack<int>& st, vector<vector<int>>& adjList) {
+        vis[node] = 1;
+        for (auto& v : adjList[node]) {
+            if (vis[v] == 1) return true;
+            if (!vis[v])
+                if (topoSort(v, vis, st, adjList)) return true;
+        }
+        st.push(node);
+        vis[node] = 2;
+        return false;
+    }
+
+   public:
+    string foreignDictionary(vector<string>& words) {
+        // DFS
+        int n = words.size();
+        vector<int> present(26, 0);
+        vector<int> vis(26, 0);
+        vector<vector<int>> adjList(26);
+        stack<int> st;
+        string ans;
+        for (auto& word : words) {
+            for (auto& ch : word) {
+                present[ch - 'a'] = 1;
+            }
+        }
+        for (int i = 0; i < n - 1; i++) {
+            string str1 = words[i];
+            string str2 = words[i + 1];
+            int len = min(str1.size(), str2.size());
+            bool foundDiff = false;
+            for (int k = 0; k < len; k++) {
+                if (str1[k] != str2[k]) {
+                    int u = str1[k] - 'a';
+                    int v = str2[k] - 'a';
+                    adjList[u].push_back(v);
+                    foundDiff = true;
+                    break;
+                }
+            }
+            if (!foundDiff && str1.size() > str2.size()) {
+                return "";
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (present[i] && !vis[i]) {
+                if (topoSort(i, vis, st, adjList)) return "";
+            }
+        }
+        while (!st.empty()) {
+            char ch = st.top() + 'a';
+            ans += ch;
+            st.pop();
+        }
+        return ans;
+    }
+};
