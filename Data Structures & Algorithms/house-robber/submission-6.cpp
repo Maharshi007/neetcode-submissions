441d24f359e3651de1354013bc6a8923bd12c8b5
Maharshi <96075286+Maharshi007@users.noreply.github.com>
2026-08-19 16:07:39 +0530

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.cpp
@@ -0,0 +1,21 @@
+// MORE SPACE OPTIMIZATION
+class Solution {
+   public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max({nums[0], nums[1]});
+        int prev2 = 0;
+        int prev1 = nums[0];
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if(i>1)
+                pick += prev2;
+            int notPick = prev1;
+            int curr = max(pick, notPick);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
